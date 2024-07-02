44b35122295327bcadd2c9f9e77e04c093d5ba6d
Ivan Bulgakov <woullocke@gmail.com>
2024-07-02 10:15:10 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Иван Булгаков on 2.7.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
